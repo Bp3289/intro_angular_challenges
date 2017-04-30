03a614757fba2cbe8866654edd4d128c1d46ae85
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-30 10:31:15 -0600

    Finished exercise #1

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,16 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  
+  this.full_name = "Billy";
+  this.age = "28";
+  this.city = "Edna";
+  this.state = "Texas";
+  this.shout = function(input){
+  			var out = input.toUpperCase() + "!";
+  			return out;
+  };
+}
