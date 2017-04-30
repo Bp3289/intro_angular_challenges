6d6c434905e273dae984bdfdd39afc0a0bc5cc95
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-30 11:03:57 -0600

    Finished exercise #2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -2,6 +2,7 @@ console.log('challenge #2 -- app.js loaded!');
 
 var app = angular.module("appTwo", []);
 app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
 
 function WelcomeController(){
   
@@ -13,4 +14,20 @@ function WelcomeController(){
   			var out = input.toUpperCase() + "!";
   			return out;
   };
+}
+
+function ClassController() {
+
+	this.class_name = "WDI3";
+	this.enrolled_students = "12";
+	this.start_date = "March 13th";
+	this.end_date = "June";
+	this.daysRemaining = function(){
+		var endDate = new Date(2017,06,02);
+		var today = new Date();
+		var oneDay = (24*60*60*1000);
+		var diffDays = Math.round(Math.abs((today.getTime() - endDate.getTime())/(oneDay)));
+		return diffDays;
+	};
+
 }
